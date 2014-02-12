0241254f33b2855f3b3876e8acb00fd0b058ced1
Jonas Johansen <jonasjohansen@gmail.com>
2014-02-13 00:02:44 +0100

    comments

--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,6 @@
+// the configuration is used like this
+//		var config = require('./config')
+//		config.websocketServerPort
 module.exports = {
 	"websocketServerUrl":"http://127.0.0.1",
 	"websocketServerPort":"8002"
--- src/server/test/PoCTest.js
+++ src/server/test/PoCTest.js
@@ -1,19 +1,36 @@
+// files necessary for unit testing
 require("../common")
 var Assert = require('assert')
+// the modules that should be tested
 var NeedsTesting = require('../NeedsTesting')
 
+// name of the module that is to be tested
 describe("PoC test", function(){
-	before(function(){});
-	after(function(){});
-	beforeEach(function(){});
-	afterEach(function(){});
+	var _moduleThatNeedsTesting = new NeedsTesting();
 
-	it("should be possible to run at unit test", function(done){
-		var moduleThatNeedsTesting = new NeedsTesting();
-		Assert.equal(moduleThatNeedsTesting.returnFalse(), false);
-		Assert.equal(moduleThatNeedsTesting.returnTrue(), true);
+	before(function(){
+		// run before all tests in this module
+	});
+	after(function(){
+		// run previous to all tests in this module
+	});
+	beforeEach(function(){
+		// run before each "it"-function
+	});
+	afterEach(function(){
+		// run previous to each "it"-function
+	});
+
+	// 1 to many "it"-functions
+	it("should be possible to run a unit test", function(done){
+		Assert.equal(_moduleThatNeedsTesting.returnFalse(), false);
+		Assert.equal(_moduleThatNeedsTesting.returnTrue(), true);
 		// test logging utility
 		logDebug("We should not log messages during a {0} :/".format("unit test"))
 		done(); // this can be used for asynchronous methods, or just left out as argument
 	});
+
+	it("should be possible to run one more unit test", function(){
+		Assert.equal("1", 1); // I love you, js!
+	});	
 });
