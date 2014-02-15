80b6613deb107bea58fc29d4fa06d3e417a8f5b2
Anders Rasmussen <anders@arit.dk>
2014-02-15 21:35:18 +0100

    m

--- src/client/js/gfxRessources.js
+++ src/client/js/gfxRessources.js
@@ -2,6 +2,14 @@ gfxRessources = {
 	createPlayerGfx: function(rsr) {
 		return rsr.image("/img/rod-mand.png", 0, 0, 20, 10);
 	},
+	createRandomPlayerGfx: function(paper) {
+		var images = ["/img/rod-mand.png"];
+		
+		var image = Math.floor(Math.random() * images.length);
+		
+		return rsr.image(image, 0, 0, 20, 10);
+	},
+	
 	createBackgroundTile: function(tile) {
 		return tile.image("/img/grass.png", 0, 0, 178, 178);
 	},
