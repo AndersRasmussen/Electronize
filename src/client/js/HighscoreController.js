0af5ee980f9249e9e4f4bb622fd8b3f8789c22a0
Jonas Johansen <jonasjohansen@gmail.com>
2014-02-15 23:58:43 +0100

    highscore data

--- src/client/js/HighscoreController.js
+++ src/client/js/HighscoreController.js
@@ -1,9 +1,24 @@
 function HighscoreController($rootScope, $scope){
 	$scope.playerid = "<Player id>";
+	$scope.highscores = [];
 	
 	$scope.$on("JOINED", function(event, player){
 		// player has joined
 		$scope.playerid = player.playerid;
 		$scope.nickname = player.nickname;
 	});
+
+	var _boardState;
+	$scope.$on("BOARDUPDATE", function(event, boardState){
+		_boardState = boardState;
+	});
+
+	var enableUpdateHighscore = function(){
+		setInterval(function(){
+			// sort players by score
+
+			$scope.highscores = [];
+		}, 2000);
+	}
+
 }
