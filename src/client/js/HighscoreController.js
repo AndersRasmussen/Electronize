624d3928c24cb72c89f7032dc569d11567ce6496
Jonas Johansen <jonasjohansen@gmail.com>
2014-02-15 11:10:08 +0100

    ddssdf

--- src/client/js/HighscoreController.js
+++ src/client/js/HighscoreController.js
@@ -1,3 +1,8 @@
 function HighscoreController($rootScope, $scope){
-
+	$scope.playerid = "<Player id>";
+	
+	$scope.$on("JOINED", function(player){
+		// player has joined
+		$scope.playerid = player.playerid;
+	});
 }
