bcf3d6fbd497735586e393e4a6a453b0853a2c32
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2014-02-15 14:10:22 +0100

    Update gfx create lib

--- src/client/js/gfxRessources.js
+++ src/client/js/gfxRessources.js
@@ -1,5 +1,12 @@
 gfxRessources = {
 	createPlayerGfx: function(rsr){
 		return rsr.image("/img/rod-mand.png", 100, 100, 165, 82);
+	},
+	createBackgroundTile: function(tile) {
+		return tile.image("/img/grass.png", 0, 0, 178, 178);
+	},
+	createHeart: function(tile) {
+		return tile.image("/img/heart.png", 0, 0, 124, 109);
 	}
+}
 }
